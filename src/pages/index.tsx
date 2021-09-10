38192d786a0b63804472f946fd95f4cb24f78db6
EK <qxjj7ay@163.com>
2021-09-11 01:14:34 +0800

    写了一个主页

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,56 @@
-import styles from './index.less';
+import React, { PureComponent } from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { DesktopOutlined, PieChartOutlined, FileOutlined, TeamOutlined, UserOutlined, } from '@ant-design/icons';
+const { Header, Content, Sider } = Layout;
+const { SubMenu } = Menu;
+import styles from './index.less'
 
-export default function IndexPage() {
-  return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
-  );
+class SiderDemo extends PureComponent {
+  state = {
+    collapsed: false,
+  };
+
+  onCollapse () {
+    this.state.collapsed ? this.setState({collapsed: false}) : this.setState({collapsed: true})
+  }
+
+  render() {
+    const { collapsed } = this.state;
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse.bind(this)}>
+          <div className={styles.logo}>react</div>
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" icon={<PieChartOutlined />}>表格</Menu.Item>
+            <Menu.Item key="2" icon={<DesktopOutlined />}>表单</Menu.Item>
+            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9" icon={<FileOutlined />}>Files</Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header className={styles.siteLayoutBackground} style={{ padding: 0, background: '#001529' }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+              <Breadcrumb.Item>Tom</Breadcrumb.Item>
+            </Breadcrumb>
+            <div className={styles.siteLayoutBackground} style={{ padding: 24, minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
 }
+
+export default SiderDemo
